86776ca818e9b628ba977a38440eb82591e21f1b
Olu-Olanubi <seyeolanubi@gmail.com>
2018-07-01 20:21:59 +0100

    Add existing file

---
+++ script.js
@@ -0,0 +1,45 @@
+
+const CACHE_NAME = 'example';
+const urlsToCache = [
+  '/',
+  '/style.css',
+  '/connection.js'
+];
+
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+		return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = ['example'];
+  event.waitUntil(
+    caches.keys().then(cacheNames => Promise.all(
+      cacheNames.map(cacheName => {
+        if (!cacheWhitelist.includes(cacheName)) {
+          return caches.delete(cacheName);
+        }
+      })
+    ))
+  );
+});
